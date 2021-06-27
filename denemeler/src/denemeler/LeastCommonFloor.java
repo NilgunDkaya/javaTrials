0c8499c142cacf2ce29b0bc345ca5b511977ad6d
NilgunDkaya <61917558+NilgunDkaya@users.noreply.github.com>
2021-06-27 17:33:39 +0300

    Add files via upload

---
+++ denemeler/src/denemeler/LeastCommonFloor.java
@@ -0,0 +1,35 @@
+package denemeler;
+
+import java.util.Scanner;
+
+public class LeastCommonFloor {
+	
+	public void findLeastCommonFloor() {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter two number for calculating okek : ");
+		Integer s1 = scan.nextInt();
+		Integer s2 = scan.nextInt();
+		
+		int n1 = s1, n2 = s2;
+		
+		int s= 1;
+		while(n1 != 1 && n2 != 1) {
+			int divisor = 2;
+			for (int i = 0; i <= (n1>n2 ? n1 : n2); i++) {
+				if(n1 % divisor == 0 || n2 % divisor == 0) {
+					s *= divisor;
+					if(n1 % divisor == 0)
+						n1 /= divisor;
+					if (n2 % divisor == 0)
+						n2 /= divisor;
+				}
+				else {
+					divisor++;
+				}
+			}
+		}
+		System.out.println("Okek for " +s1 +" and " +s2 +" is " +s);
+	}
+
+}
