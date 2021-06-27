f6196a554cb241a7010cc9ed09b18873f61726f9
NilgunDkaya <61917558+NilgunDkaya@users.noreply.github.com>
2021-06-27 17:44:41 +0300

    Add files via upload

---
+++ denemeler/src/denemeler/GreatestCommonDivisor.java
@@ -0,0 +1,36 @@
+package denemeler;
+
+import java.util.Scanner;
+
+public class GreatestCommonDivisor {
+	
+	public void findGreatestCommonDivisor() {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter two number for calculating okek : ");
+		Integer s1 = scan.nextInt();
+		Integer s2 = scan.nextInt();
+		
+		int n1 = s1, n2 = s2;
+		
+		int s= 1;
+		int divisor = 2;
+		
+		while(n1 != 1 && n2 != 1) {
+			for (int i = 0; i <= (n1>n2 ? n1 : n2); i++) {
+				if(n1 % divisor == 0 || n2 % divisor == 0) {
+					if(n1 % divisor == 0 && n2 % divisor == 0)
+						s *= divisor;
+					if(n1 % divisor == 0)
+						n1 /= divisor;
+					if (n2 % divisor == 0)
+						n2 /= divisor;
+				}
+				else
+					divisor++;
+			}
+		}
+		System.out.println("The greatest common divisor for " +s1 +" and " +s2 +" is " +s);
+	}
+
+}
